af2c61596d65caf57681808905b47a5d6911bc26
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-06-25 16:12:07 -0500

    Adding server.js, htmlRoutes.js, and apiRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const { Router } = require("express");
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports = Router;
