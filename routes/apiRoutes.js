00ad6945323f731368d73ef1226cf9f1f36c4171
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-09-28 16:59:11 -0500

    Updating code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const store = require("../db/store")
 
-let db = require("../db/db.json");
-router.get("/api/notes", function(req, res) {
-    db = json.parse(fs.readFileSync("./db/db.json", "UTF-8"))
-    console.log(db)
-    res.json(db)
+// GET "/api/notes" responds with all notes from the database
+router.get("/notes", (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
 });
 
-router.post("/api/notes", function(req, res) {
-    db.push(req.body)
-    fs.writeFileSync("./db/db.json", db, function(){
-        console.log(db)
-        res.json(db)
-    })
+router.post("/notes", (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
 });
 
-router.delete("/api/notes/:id", function(req, res) {
-    var deleteNoteID = req.params.id
-    let data = []
-    for (let i = 0; i < data.length; i++) {
-        if (db[i].id !== deleteNoteID) {
-            data.push(db[i])
-        }
-    }
-    db = data
-    fs.writeFileSync("./db/db.json", db, function(){
-        console.log(db)
-        res.json(db)
-    })
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -12,6 +12,7 @@ app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
 
 app.use(express.static("public"));
+
 app.listen(PORT, ()=>{
     console.log("app listening on port", PORT)
 });
